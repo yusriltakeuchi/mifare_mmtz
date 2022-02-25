e4e2665a2e801e5065b5c1cfd933b1de12e8c904
Yusril Rapsanjani <yusriltakeuchi@gmail.com>
2022-02-25 23:18:10 +0700

    Feat: Implements Get NFC Status

--- android/src/main/kotlin/com/yurani/mifare_mmtz/MifareMmtzPlugin.kt
+++ android/src/main/kotlin/com/yurani/mifare_mmtz/MifareMmtzPlugin.kt
@@ -1,5 +1,8 @@
 package com.yurani.mifare_mmtz
 
+import android.app.Activity
+import android.nfc.NfcAdapter
+import android.nfc.tech.MifareClassic
 import androidx.annotation.NonNull
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
@@ -16,18 +19,37 @@ class MifareMmtzPlugin: FlutterPlugin, MethodCallHandler {
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
+  private lateinit var activity: Activity
+  private var mNfcAdapter: NfcAdapter? = null
+  private lateinit var mifareClassic: MifareClassic
+  private val flag = NfcAdapter.FLAG_READER_NFC_A
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "mifare_mmtz")
     channel.setMethodCallHandler(this)
+    mNfcAdapter = NfcAdapter.getDefaultAdapter(flutterPluginBinding.applicationContext)
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    when (call.method) {
+      "isNFCEnable" -> {
+        isNFCEnabled(result)
+      }
+      else -> {
+        result.notImplemented()
+      }
     }
+  }
+
+  private fun isNFCEnabled(result: Result) {
+    val message: String = if (mNfcAdapter != null && mNfcAdapter!!.isEnabled) {
+      "Enabled"
+    } else if (mNfcAdapter != null && !mNfcAdapter!!.isEnabled) {
+      "NotEnabled"
+    } else {
+      "NotSupported"
+    }
+    result.success(message)
   }
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
--- lib/mifare_mmtz.dart
+++ lib/mifare_mmtz.dart
@@ -3,12 +3,32 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+enum NFCStatus {
+  Enabled,
+  NotEnabled,
+  NotSupported
+}
 class MifareMmtz {
-  static const MethodChannel _channel =
-      const MethodChannel('mifare_mmtz');
+  
+  /// Current channel
+  static const MethodChannel _channel = const MethodChannel('mifare_mmtz');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  /// Check current availability of NFC
+  static Future<NFCStatus> isNFCEnable() async {
+    final response = await _channel.invokeMethod("isNFCEnable") as String;
+    return _mapEnableStats(response);
+  }
+
+  static NFCStatus _mapEnableStats(String status) {
+    switch (status) {
+      case "Enabled":
+        return NFCStatus.Enabled;
+      case "NotEnabled":
+        return NFCStatus.NotEnabled;
+      case "NotSupported":
+        return NFCStatus.NotSupported;
+      default:
+        return NFCStatus.NotSupported;
+    }
   }
 }
